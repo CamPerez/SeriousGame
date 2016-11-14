da148cf10192a41f92c00884cc801154447d1e39
camy110 <camperezarevalo@hotmail.com>
2016-11-14 13:50:05 +0100

    Tap with notes and activator working

--- SeriousGame/Assets/Scripts/Activator.cs
+++ SeriousGame/Assets/Scripts/Activator.cs
@@ -3,32 +3,50 @@ using System.Collections;
 
 public class Activator : MonoBehaviour {
 
+	private SpriteRenderer sr;
+
+	private Color colorWhenPressed = new Color(0,0,0);
+	private Color oldColor;
+
 	public KeyCode key;
 	bool active = false;
-	GameObject note;
+
+	private GameObject note;
+
+	void Awake () {
+		sr = GetComponent<SpriteRenderer> ();
+	}
 
 	// Use this for initialization
 	void Start () {
-	
+		oldColor = sr.color;
+		NotificationCenter.DefaultCenter().AddObserver (this, "NotePressedCorrectly"); //Pendientes de mensaje de nota tocada dentro de activador
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetMouseButtonDown (0) && active){ //Si se ha tocado la tecla y está activo (hay un objeto pasando por el activador)
-			Destroy (note); //Destruimos la nota
-		}
+
 	}
 
 	//Se detecta un elemento tocando al activador
 	void OnTriggerEnter2D(Collider2D col){
 		active = true;
-		if(col.gameObject.tag == "Note"){ //Si el elemento que nos toca es una nota se guarda
-			note = col.gameObject;
-		}
 	}
 
 
 	void OnTriggerExit2D(Collider2D col){
 		active = false;
 	}
+
+	public void SetColorChange(){
+		StartCoroutine (Pressed ());
+	}
+		
+	IEnumerator Pressed(){
+		sr.color = colorWhenPressed;
+		yield return new WaitForSeconds (0.05f);
+		sr.color = oldColor;
+	}
+
+
 }
--- SeriousGame/Assets/Scripts/Note.cs
+++ SeriousGame/Assets/Scripts/Note.cs
@@ -5,6 +5,13 @@ public class Note : MonoBehaviour {
 
 	Rigidbody2D rb;
 	public float speed = 2;
+	private bool inside = false;
+
+
+	private RuntimePlatform platform = Application.platform;
+	private GameObject note;
+	private GameObject activator;
+	private Activator activatorScript;
 
 	void Awake(){
 		rb = GetComponent<Rigidbody2D> ();
@@ -14,9 +21,49 @@ public class Note : MonoBehaviour {
 	void Start () {
 		rb.velocity = new Vector2 (0, -speed);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void Update(){
+		if(platform == RuntimePlatform.Android || platform == RuntimePlatform.IPhonePlayer){
+			if(Input.touchCount > 0) {
+				if(Input.GetTouch(0).phase == TouchPhase.Began){
+					checkTouch(Input.GetTouch(0).position);
+				}
+			}
+		}else if(platform == RuntimePlatform.WindowsEditor){
+			if(Input.GetMouseButtonDown(0)) {
+				checkTouch(Input.mousePosition);
+			}
+		}
 	}
+
+	void checkTouch(Vector3 pos){
+		Vector3 wp = Camera.main.ScreenToWorldPoint(pos);
+		Vector2 touchPos = new Vector2(wp.x, wp.y);
+		Collider2D hit = Physics2D.OverlapPoint(touchPos);
+
+		if(hit){
+			if (hit.transform.gameObject.tag == "Note") {
+				note = hit.transform.gameObject;
+			}
+			if(inside == true && gameObject.name == note.name){
+				NotificationCenter.DefaultCenter ().PostNotification (this, "NotePressedCorrectly"); //La nota ha sido pulsada cuando pasa por un activador
+				activator.GetComponent <Activator>().SetColorChange ();
+				Destroy (note);
+			}
+		}
+	}
+
+
+
+	//Se detecta un elemento tocando al activador
+	void OnTriggerEnter2D(Collider2D col){
+		inside = true;
+		activator = col.gameObject;
+	}
+
+
+	void OnTriggerExit2D(Collider2D col){
+		inside = false;
+	}
+		
 }
