e34da468f3a07574acc2d3a20b6becc457674083
camy110 <camperezarevalo@hotmail.com>
2016-11-27 16:20:03 +0100

    score bar added and working

---
+++ SeriousGame/Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	[SerializeField]
+	private Stat points;
+
+
+	void Awake(){
+		points.InitializeValues ();
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetKeyDown (KeyCode.Q)){
+			points.CurrentVal -= 10;
+		}
+
+		if(Input.GetKeyDown (KeyCode.W)){
+			points.CurrentVal += 10;
+		}
+	}
+}
